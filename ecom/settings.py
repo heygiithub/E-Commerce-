3648654005a5e568d4e7f740bd59178ab42a4d7e
heygiithub <shaikhabrar8929@gmail.com>
2026-01-31 14:12:50 +0530

    removed broken logging config

--- ecom/settings.py
+++ ecom/settings.py
@@ -189,18 +189,18 @@ CORS_ALLOWED_ORIGINS = ["https://frontend-uctm.onrender.com",]
 CORS_ALLOW_CREDENTIALS = True
 
 # LOGGING FOR DUBUGGING
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler'},
-    'root':{
-        'handlers':['console'],
-        'level':'INFO',},
-    }
+# LOGGINNG = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler'},
+#     'root':{
+#         'handlers':['console'],
+#         'level':'INFO',},
+#     }
     
-}
+# }
 
 
 
